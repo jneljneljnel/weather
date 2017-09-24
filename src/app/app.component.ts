1609acdd2b990d984c517585df52e62c7980ba49
Jeremy <Jeremy>
2017-09-23 23:47:49 -0700

    sec commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map'
+import * as moment from 'moment';
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = '';
   data: any = {};
@@ -15,16 +18,22 @@ export class AppComponent {
   lon: number = 0; 
 
   constructor(private http: Http ){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(position => {
-        this.lat = position.coords.latitude;
-        this.lon = position.coords.longitude;
-        this.getWeather(this.lat,this.lon);
-      });
-   }
-   else {
-     this.title = 'Please enable location services.'; 
-   }
+      navigator.geolocation.getCurrentPosition(pos => {  
+        this.getWeather(pos.coords.latitude,pos.coords.longitude);  
+      },(error =>{
+        this.getWeather(33.71,-117.76);
+      }));     
+  }
+
+  getWeather(lat, lon) {
+    this.getData(lat, lon).subscribe(data => {
+      this.setBgImg(data.city.name);
+      this.divideArray(data);
+      this.formatDate(data);
+      this.formatIcon(data);
+      this.city = data.city.name;
+      this.data = data      
+    });
   }
 
   formApi(lat, lon){
@@ -32,19 +41,86 @@ export class AppComponent {
     return apiUrl;
   }
 
+  setBgImg(city){
+    let body = document.getElementsByTagName('body')[0];
+    if(city === "Orange County"){
+          body.classList.add('irvine');
+    }
+    else if(city === "Los Angeles"){
+          body.classList.add('la');
+    }
+    else if(moment().isAfter(moment('07:00', 'hh:mm'))){
+          body.classList.add('night');
+    } 
+    else {
+      body.classList.add('day');
+    }
+  };
+
+  formatDate(data){
+      data.list.forEach( l => {
+         l.dt_txt =(moment(l.dt_txt).format('MMM Do h:mm a'));      
+      }) 
+      return data
+  }
+
   getData(lat, lon){
     return this.http.get(this.formApi(lat, lon))
       .map((res:Response)=>res.json())
   }
 
-  divideArray(list, int){
-    var arr = [];
-    list.forEach(element => {
-      console.log(element);
-    });
+  formatIcon(data){
+    data.list.forEach( el =>{
+      el.weather.forEach(l =>{
+        if (l.description == 'Clear Sky'){
+          l.icon = 'wi-day-sunny'
+        }
+      });
+    })
   }
 
-  getWeather(lat, lon) {
-    this.getData(lat, lon).subscribe(data => {console.log(data); this.city = data.city.name; this.data = data})
+  getIcon(desc){
+    let icon
+    if(desc == 'clear sky'){
+      icon =  {'wi-day-sunny':true}
+    }
+    if(desc == 'few clouds'){
+      icon =  {'wi-cloud':true}
+    }
+    if(desc == 'scattered clouds'){
+      icon =  {'wi-cloudy':true}
+    }
+    if(desc == 'broken clouds'){
+      icon =  {'wi-cloudy':true}
+    }
+    if(desc == 'shower rain'){
+      icon =  {'wi-rain-wind':true}
+    }
+    if(desc == 'rain'){
+      icon =  {'wi-rain':true}
+    }
+    if(desc == 'thunderstorm'){
+      icon =  {'wi-thunderstorm':true}
+    }
+    if(desc == 'snow'){
+      icon =  {'wi-snow':true}
+    }
+    if(desc == 'mist'){
+      icon =  {'wi-rain-mix':true}
+    }
+    return icon
   }
+
+  divideArray(data){
+    var arr = [];
+    data.list.forEach( el => {
+     if(data.list.indexOf(el) % 8 == 0 || data.list.indexOf(el) == 0 ){
+       arr.push(el);
+     };
+    });
+    data.list = arr;
+    return data;
+  }
+
+  
 }
